a5f3a47aa5a14a58cee7b544a0f5305df8aa2f42
samiali12349934-hash <samiali12349934@gmail.com>
2026-02-27 11:25:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-// Empty for now, you can add global functions like theme or music here
-console.log("Our World App JS loaded");
+// Background Music
+let music = document.getElementById("bgMusic");
+function toggleMusic() {
+  if (music.paused) music.play();
+  else music.pause();
+}
+
+// Dark / Light Mode
+let darkMode = true;
+function toggleTheme() {
+  if (darkMode) {
+    document.body.style.background = "#111";
+    document.body.style.color = "white";
+  } else {
+    document.body.style.background = "";
+    document.body.style.color = "";
+  }
+  darkMode = !darkMode;
+}
+
+// Change Background Color
+function changeBgColor(color){
+  document.body.style.background = color;
+}
